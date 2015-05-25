158ae8a41373c61acc444a92140f5136e8ddad9b
astronaughts <hello@astronaughts.net>
2015-05-25 16:47:42 +0900

    fix bugs

--- StarCounter.py
+++ StarCounter.py
@@ -2,5 +2,7 @@ import sublime, sublime_plugin
 
 class StarCounterCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        for num, region in enumerate(self.view.find_all('★')):
-            self.view.replace(edit, region, str(num + 1))
+        window = sublime.active_window()
+        view = window.active_view()
+        for num, region in enumerate(view.find_all('★')):
+            view.replace(edit, region, str(num + 1))
